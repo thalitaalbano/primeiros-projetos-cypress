dcdb110e985622952b7a40cd8ab710482ad834ab
Thalita Albano <thalita_albano@hotmail.com>
2025-09-22 21:49:30 -0300

    .

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -2,60 +2,36 @@ import userData from '../fixtures/user-data.json'
 import LoginPage from '../pages/loginPage'
 import DashboardPage from '../pages/dashboardPage'
 import MenuPage from '../pages/menuPage'
+import MyInfoPage from '../pages/myInfoPage'
 
 const loginPage = new LoginPage()
 const dashboardPage = new DashboardPage()
 const menuPage = new MenuPage()
+const myInfoPage = new MyInfoPage()
 
 describe('Orange HRM Tests', () => {
-
-const selectorsList = {  
-  myInfoButton: '[href="/web/index.php/pim/viewMyDetails"]',
-  firstNameField: "[name='firstName']",
-  lastNameField: "[name='lastName']",
-  genericField: ".oxd-input--active",
-  dateField: "[placeholder='yyyy-dd-mm']",
-  genericCombobox: ".oxd-select-text--arrow",
-  secondItemCombobox: ".oxd-select-dropdown > :nth-child(2)",
-  thirdItemCombobox: ".oxd-select-dropsown > :nth-child(3)",
-  dateCloseButton: ".--close",
-  submitButton: "[type='submit']"
-
-}
 
   it.only('User Info Update - Success', () => {
     loginPage.accessLoginPage()
     loginPage.loginWithAnyUser(userData.userSuccess.username, userData.userSuccess.password)
-    
+
     dashboardPage.checkDashboardPage()
+
     menuPage.accessMyInfo()
 
-
-    
-    cy.get(selectorsList.firstNameField).clear().type('FirstNameTest')
-    cy.get(selectorsList.lastNameField).clear().type('LastNameTest')
-    cy.get(selectorsList.genericField).eq(3).clear().type('Employee')
-    cy.get(selectorsList.genericField).eq(4).clear().type('OtherId')
-    cy.get(selectorsList.genericField).eq(5).clear({ force: true }).type('DriverLicenseTest')
-    cy.get(selectorsList.genericField).eq(6).clear().type('2025-03-10')
-    cy.get(selectorsList.dateCloseButton).click()
-    cy.get(selectorsList.submitButton).eq(0).click({force: true})
-    cy.get('body').should('contain', 'Successfully Updated')
-    cy.get('.oxd-toast-close')
-
-    cy.get(selectorsList.genericCombobox).eq(0).click()
-    cy.get('.oxd-select-dropdown > :nth-child(3)').click()
-    cy.get(selectorsList.genericCombobox).eq(1).click()
-    cy.get('.oxd-select-dropdown > :nth-child(2)').click()
-    
-    
-
+    myInfoPage.fillPersonalDetails('First Name', 'Last Name')
+    myInfoPage.fillEmployeeDetails('Employee Id', 'otherId', 'Drivers Number', '2025-07-29')
+    myInfoPage.fillStatus()
+    myInfoPage.saveForm()
   })
+
   it('Login - Fail', () => {
     cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userFail.username)
     cy.get(selectorsList.passwordField).type(userData.userFail.password)
     cy.get(selectorsList.loginButton).click()
     cy.get(selectorsList.wrongCredentialAlert)
+
   })
+
 })
---
+++ cypress/pages/myInfoPage.js
@@ -0,0 +1,52 @@
+class MyInfoPage{
+    selectorsList(){
+        const selectors = {
+            myInfoButton: '[href="/web/index.php/pim/viewMyDetails"]',
+            firstNameField: "[name='firstName']",
+            lastNameField: "[name='lastName']",
+            genericField: ".oxd-input--active",
+            dateField: "[placeholder='yyyy-dd-mm']",
+            genericCombobox: ".oxd-select-text--arrow",
+            secondItemCombobox: ".oxd-select-dropdown > :nth-child(2)",
+            thirdItemCombobox: ".oxd-select-dropdown > :nth-child(3)",
+            dateCloseButton: ".--close",
+            submitButton: "[type='submit']"
+
+        }
+
+        return selectors
+    }
+
+    fillPersonalDetails(firstName, lastName){
+        cy.get(this.selectorsList().firstNameField).clear().type(firstName)
+        cy.get(this.selectorsList().lastNameField).clear().type(lastName)
+    }
+
+    fillEmployeeDetails(employeeId, otherId, driversLicenseNumber, expyriDate){
+        cy.get(this.selectorsList().genericField).eq(3).clear().type(employeeId)
+        cy.get(this.selectorsList().genericField).eq(4).clear().type(otherId)
+        cy.get(this.selectorsList().genericField).eq(5).clear({ force: true }).type(driversLicenseNumber)
+        cy.get(this.selectorsList().genericField).eq(6).clear().type(expyriDate)
+        cy.get(this.selectorsList().dateCloseButton).click()
+
+    }
+
+    saveForm() {
+    cy.get(this.selectorsList().submitButton).eq(0).click({force:true})
+    cy.get('.oxd-toast').should('contain', 'Successfully Saved')
+    cy.get('.oxd-toast-close')
+    }
+    
+    fillStatus() {
+        cy.get(this.selectorsList().genericCombobox).eq(0).click({force: true})
+        cy.get(this.selectorsList().secondItemCombobox).click()
+        cy.get(this.selectorsList().genericCombobox).eq(1).click({force: true})
+        cy.get(this.selectorsList().thirdItemCombobox).click()
+
+    }
+
+
+
+}
+
+export default MyInfoPage
